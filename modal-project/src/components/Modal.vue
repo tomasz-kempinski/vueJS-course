5a6ff2cf20625b1d9a7f02b4c3a29b4ec2ece57c
Tomasz Kempinski <tomasz.kempinski91@gmail.com>
2021-07-18 21:07:57 +0200

    Add component styles and global styles

--- modal-project/src/components/Modal.vue
+++ modal-project/src/components/Modal.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="backdrop">
     <div class="modal">
+      <h1>Modal title</h1>
       <p>modal content</p>
     </div>
   </div>
 </template>
 
+// scoped spowoduje że style zostanie zastosowany tylko do tego componentu
 <style>
+/* <style scoped> */
 .modal {
   width: 400px;
   padding: 20px;
@@ -20,5 +23,19 @@
   background: rgba(0, 0, 0, 0.5);
   width: 100%;
   height: 100%;
+}
+/* h1{
+    color: aqua;
+    border: none;
+    padding: 0;
+} */
+/* alternatywą dla 'scoped' jest przypisanie clasy modułu do elementu */
+.modal h1 {
+  color: aqua;
+  border: none;
+  padding: 0;
+}
+.modal p {
+  font-style: normal;
 }
 </style>
